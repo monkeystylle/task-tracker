e760b39de07664f6b917e84ba652fc4e774173e5
meng <monkeystylle@gmail.com>
2024-12-29 03:55:12 +0800

    added use-action-feedback.ts

--- src/features/ticket/actions/upsert-ticket.ts
+++ src/features/ticket/actions/upsert-ticket.ts
@@ -2,24 +2,39 @@
 
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
+import { z } from 'zod';
+import {
+  ActionState,
+  fromErrorToActionState,
+  toActionState,
+} from '@/components/form/utils/to-action-state';
 import { prisma } from '@/lib/prisma';
 import { ticketPath, ticketsPath } from '@/paths';
 
+const upsertTicketSchema = z.object({
+  title: z.string().min(1).max(191),
+  content: z.string().min(1).max(1024),
+});
+
 export const upsertTicket = async (
   id: string | undefined,
-  _actionState: { message: string },
+  _actionState: ActionState,
   formData: FormData
 ) => {
-  const data = {
-    title: formData.get('title') as string,
-    content: formData.get('content') as string,
-  };
+  try {
+    const data = upsertTicketSchema.parse({
+      title: formData.get('title'),
+      content: formData.get('content'),
+    });
 
-  await prisma.ticket.upsert({
-    where: { id: id || '' },
-    update: data,
-    create: data,
-  });
+    await prisma.ticket.upsert({
+      where: { id: id || '' },
+      update: data,
+      create: data,
+    });
+  } catch (error) {
+    return fromErrorToActionState(error, formData);
+  }
 
   revalidatePath(ticketsPath());
 
@@ -27,5 +42,5 @@ export const upsertTicket = async (
     redirect(ticketPath(id));
   }
 
-  return { message: 'Ticket created' };
+  return toActionState('SUCCESS', 'Ticket created');
 };
--- src/features/ticket/components/ticket-upsert-form.tsx
+++ src/features/ticket/components/ticket-upsert-form.tsx
@@ -2,7 +2,10 @@
 
 import { Ticket } from '@prisma/client';
 import { useActionState } from 'react';
+import { FieldError } from '@/components/form/field-error';
+import { useActionFeedback } from '@/components/form/hooks/use-action-feedback';
 import { SubmitButton } from '@/components/form/submit-button';
+import { EMPTY_ACTION_STATE } from '@/components/form/utils/to-action-state';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { Textarea } from '@/components/ui/textarea';
@@ -15,20 +18,44 @@ type TicketUpsertFormProps = {
 const TicketUpsertForm = ({ ticket }: TicketUpsertFormProps) => {
   const [actionState, action] = useActionState(
     upsertTicket.bind(null, ticket?.id),
-    { message: '' }
+    EMPTY_ACTION_STATE
   );
+
+  useActionFeedback(actionState, {
+    onSuccess: ({ actionState }) => {
+      console.log(actionState.message);
+      // TODO optionally handle success
+    },
+    onError: ({ actionState }) => {
+      console.log(actionState.message);
+      // TODO optionally handle error
+    },
+  });
 
   return (
     <form action={action} className="flex flex-col gap-y-2">
       <Label htmlFor="title">Title</Label>
-      <Input id="title" name="title" type="text" defaultValue={ticket?.title} />
+      <Input
+        id="title"
+        name="title"
+        type="text"
+        defaultValue={
+          (actionState.payload?.get('title') as string) ?? ticket?.title
+        }
+      />
+      <FieldError actionState={actionState} name="title" />
 
       <Label htmlFor="content">Content</Label>
-      <Textarea id="content" name="content" defaultValue={ticket?.content} />
+      <Textarea
+        id="content"
+        name="content"
+        defaultValue={
+          (actionState.payload?.get('content') as string) ?? ticket?.content
+        }
+      />
+      <FieldError actionState={actionState} name="content" />
 
       <SubmitButton label={ticket ? 'Edit' : 'Create'} />
-
-      {actionState.message}
     </form>
   );
 };
