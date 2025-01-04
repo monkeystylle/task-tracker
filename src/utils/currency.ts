337eca023a92a1b144409a46976cf56e5689d8fe
meng <monkeystylle@gmail.com>
2025-01-04 22:01:55 +0800

    save custom datepicker update

--- src/utils/currency.ts
+++ src/utils/currency.ts
@@ -1,8 +1,12 @@
 import { MyBig } from '@/lib/big';
 
+// Converting \$10.99 to cents for storage
+// const amountInCents = toCent(10.99);  // Returns 1099
 export const toCent = (amount: number) =>
   new MyBig(amount).mul(100).round(2).toNumber();
 
+// Converting 1099 cents back to dollars
+// const amountInDollars = fromCent(1099);  // Returns 10.99
 export const fromCent = (amount: number) =>
   new MyBig(amount).div(100).round(2).toNumber();
 
