8e93b723fb46830e3b96f0517c6ac5a3a2c7c39f
meng <monkeystylle@gmail.com>
2025-01-20 17:22:24 +0800

    added sidebar

--- src/features/ticket/queries/get-tickets.ts
+++ src/features/ticket/queries/get-tickets.ts
@@ -17,3 +17,9 @@ export const getTickets = async (userId: string | undefined) => {
     },
   });
 };
+
+//Files in the queries folder (like get-ticket.ts) that are only imported
+// and used by server components don't need the 'use server' directive
+
+//If they're only called from server components or other server-side code,
+// they're already running on the server
